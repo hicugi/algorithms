bd8970bc3ebe056623fdcb6f703f3ac4a806934a
Mag <hicugi@gmail.com>
2023-04-08 14:15:05 +0600

    fix: testing array's

--- src/75/easy/mergeTwoLists.test.mjs
+++ src/75/easy/mergeTwoLists.test.mjs
@@ -2,13 +2,13 @@ const { mergeTwoLists } = require("./mergeTwoLists.mjs");
 
 test("Example 1", () => {
   const result = mergeTwoLists([1, 2, 4], [1, 3, 4]);
-  expect(result.join("|")).toBe([1, 1, 2, 3, 4, 4].join("|"));
+  expect(result).toEqual([1, 1, 2, 3, 4, 4]);
 });
 test("Example 2", () => {
   const result = mergeTwoLists([], []);
-  expect(result.join("|")).toBe("");
+  expect(result).toEqual([]);
 });
 test("Example 3", () => {
   const result = mergeTwoLists([], [0]);
-  expect(result.join("|")).toBe("0");
+  expect(result).toEqual([0]);
 });
--- src/75/easy/middleNode.test.mjs
+++ src/75/easy/middleNode.test.mjs
@@ -3,21 +3,21 @@ const { middleNode } = require("./middleNode.mjs");
 describe("Custom:", () => {
   test("empty array", () => {
     const result = middleNode([]);
-    expect(result.join(",")).toBe([].join(","));
+    expect(result).toEqual([]);
   });
   test("one element array", () => {
     const result = middleNode([1]);
-    expect(result.join(",")).toBe([1].join(","));
+    expect(result).toEqual([1]);
   });
 });
 
 describe("Official:", () => {
   test("Example 1", () => {
     const result = middleNode([1, 2, 3, 4, 5]);
-    expect(result.join(",")).toBe([3, 4, 5].join(","));
+    expect(result).toEqual([3, 4, 5]);
   });
   test("Example 2", () => {
     const result = middleNode([1, 2, 3, 4, 5, 6]);
-    expect(result.join(",")).toBe([4, 5, 6].join(","));
+    expect(result).toEqual([4, 5, 6]);
   });
 });
