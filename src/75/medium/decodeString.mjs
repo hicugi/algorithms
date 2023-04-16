6645cc81964d454171a1b5044f5658ee31fadba7
Mag <hicugi@gmail.com>
2023-04-16 15:29:58 +0600

    fix: optimize script & remove extra comments

--- src/75/medium/decodeString.mjs
+++ src/75/medium/decodeString.mjs
@@ -2,84 +2,40 @@
  * @param {string} s
  * @return {string}
  */
-const getChild = (s, i) => {
-  let endLine = 0;
-  let brackets = 0;
+const isNumber = (n) => Number(n) >= 0;
 
-  for (let j = i; j < s.length; j++) {
-    const childChar = s[j];
-    if (childChar === "[") {
-      brackets += 1;
-      continue;
-    }
-    if (childChar === "]") {
-      if (brackets === 0) {
-        endLine = j + 1;
-        break;
-      }
-
-      brackets--;
-    }
+const decodeString = (s) => {
+  if (s.length === 1) {
+    return isNumber(s) ? "" : s;
   }
 
-  return s.substring(i, endLine - 1);
-};
-
-const decodeString = (s, level = 0) => {
-  let result = "";
-  let letter = "";
-  let num = "";
+  const stack = [];
 
   for (let i = 0; i < s.length; i++) {
-    let char = s[i];
-    const value = Number(char);
-
-    // num
-    if (typeof num === "string" && value >= 0) {
-      num += char;
+    const char = s[i];
+    if (char !== "]") {
+      stack.push(char);
       continue;
     }
 
-    // after [
-    if (value >= 0) {
-      const child = getChild(s, i);
-      letter = `${letter}${decodeString(child, level + 1)}`;
-      i += child.length - 1;
-      continue;
+    let str = "";
+
+    while (true) {
+      const char = stack.pop();
+      if (char === "[") break;
+      str = char + str;
     }
 
-    // chars: [ ] a-z
-    switch (char) {
-      case "[":
-        result += letter;
-        letter = "";
-        num = Number(num);
-        continue;
-      case "]":
-        result += letter.repeat(num);
-        num = "";
-        letter = "";
-        continue;
-      default:
-        letter += char;
+    let n = "";
+    while (isNumber(stack[stack.length - 1])) {
+      const number = stack.pop();
+      n = number + n;
     }
+
+    stack.push(str.repeat(Number(n)));
   }
 
-  return result + letter;
+  return stack.join("");
 };
 
 module.exports = { decodeString };
-
-// 3[a2[c]]     > accaccacc
-
-// 3[z]2[2[y]pq4[2[jk]e1[f]]]ef
-//     2[2[y]pq4[2[jk]e1[f]]]ef     zzz
-//           pq4[2[jk]e1[f]]]ef     zzz yy
-//             4[2[jk]e1[f]]]ef     zzz yy pq
-//             4[     e1[f]]]ef     zzz yy pq jkjk
-//             4[      1[f]]]ef     zzz yy pq jkjk e
-//             4[          ]]ef     zzz yy pq jkjk f
-//             4[  jkjkef  ]]ef     zzz yy pq jkjkef
-
-// 3[z]2[2[y]pq4[ 2[jk]e1[f]] ]ef   jkjkef
-// 3[z]2[ 2[y]pq4[jkjkef] ]ef       yypqjkjkefjkjkefjkjkefjkjkef
--- src/75/medium/decodeString.test.mjs
+++ src/75/medium/decodeString.test.mjs
@@ -1,23 +1,36 @@
 const { decodeString } = require("./decodeString.mjs");
 
-test("Example 1", () => {
-  const result = decodeString("3[a]2[bc]");
-  expect(result).toBe("aaabcbc");
-});
-test("Example 2", () => {
-  const result = decodeString("3[a2[c]]");
-  expect(result).toBe("accaccacc");
-});
-test("Example 3", () => {
-  const result = decodeString("2[abc]3[cd]ef");
-  expect(result).toBe("abcabccdcdcdef");
+describe("Custom", () => {
+  test("just a character", () => {
+    const result = decodeString("a");
+    expect(result).toBe("a");
+  });
+  test("just a number", () => {
+    const result = decodeString("1");
+    expect(result).toBe("");
+  });
 });
 
-test("Testcase 23", () => {
-  const result = decodeString("abc3[cd]xyz");
-  expect(result).toBe("abccdcdcdxyz");
-});
-test("Testcase 28", () => {
-  const result = decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef");
-  expect(result).toBe("zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef");
+describe("Testcase", () => {
+  test("Example 1", () => {
+    const result = decodeString("3[a]2[bc]");
+    expect(result).toBe("aaabcbc");
+  });
+  test("Example 2", () => {
+    const result = decodeString("3[a2[c]]");
+    expect(result).toBe("accaccacc");
+  });
+  test("Example 3", () => {
+    const result = decodeString("2[abc]3[cd]ef");
+    expect(result).toBe("abcabccdcdcdef");
+  });
+
+  test("Testcase 23", () => {
+    const result = decodeString("abc3[cd]xyz");
+    expect(result).toBe("abccdcdcdxyz");
+  });
+  test("Testcase 28", () => {
+    const result = decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef");
+    expect(result).toBe("zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef");
+  });
 });
