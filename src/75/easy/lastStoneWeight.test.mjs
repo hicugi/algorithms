a3d450b5a3fdeedfba7e0b34db2cdd41152f28be
Mag <hicugi@gmail.com>
2023-04-17 13:30:22 +0600

    fix: naming for example test

--- src/75/easy/lastStoneWeight.test.mjs
+++ src/75/easy/lastStoneWeight.test.mjs
@@ -4,7 +4,7 @@ test("Example 1", () => {
   const result = lastStoneWeight([2, 7, 4, 1, 8, 1]);
   expect(result).toBe(1);
 });
-test("Example 1", () => {
+test("Example 2", () => {
   const result = lastStoneWeight([1]);
   expect(result).toBe(1);
 });
