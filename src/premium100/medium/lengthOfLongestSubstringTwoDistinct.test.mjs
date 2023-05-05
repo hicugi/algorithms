07fc6b1b4229ac819409aee9b29f6415249a5687
Mag <magzhan_birgebaiuly@epam.com>
2023-05-05 20:52:39 +0600

    fix: exclude comment

--- src/premium100/medium/lengthOfLongestSubstringTwoDistinct.test.mjs
+++ src/premium100/medium/lengthOfLongestSubstringTwoDistinct.test.mjs
@@ -1,10 +1,5 @@
 const { lengthOfLongestSubstringTwoDistinct } = require("./lengthOfLongestSubstringTwoDistinct.mjs");
 
-// test("Custome", () => {
-//   const result = lengthOfLongestSubstringTwoDistinct("ecebaqweraaaassszxc");
-//   expect(result).toBe(3);
-// });
-
 test("Example 1", () => {
   const result = lengthOfLongestSubstringTwoDistinct("eceba");
   expect(result).toBe(3);
